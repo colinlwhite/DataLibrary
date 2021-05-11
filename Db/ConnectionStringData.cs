dd2d5c3cd5537caad8c30534ba1b8da50a4d0d9e
Colin White <clwhite814@gmail.com>
2021-05-11 11:33:28 -0500

    we've connected to our database

---
+++ Db/ConnectionStringData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary.Db
+{
+    public class ConnectionStringData
+    {
+        // TODO - Move Connection Name to appSettings.json
+        public string SqlConnectionName { get; set; } = "default";
+    }
+}
